ae848da73ce2ee4935ea09b5afefc1b9a35d4dbe
QUENTIN.Rigagneau <rga.quentin@gmail.com>
2025-09-01 16:01:43 +0200

    Add texture loader and GPU texture resource

---
+++ VraKtalEngine/src/graphics/loaders/textureLoader.h
@@ -0,0 +1,34 @@
+#ifndef VRAKTAL_GRAPHICS_LOADERS_TEXTURE_LOADER_H
+#define VRAKTAL_GRAPHICS_LOADERS_TEXTURE_LOADER_H
+#pragma once
+
+#include "../resources/texture.h"
+#include <vector>
+#include <string>
+
+struct stbi_uc;
+
+namespace rhi::vulkan { class GpuDeviceVulkan; }
+namespace graphics::scene { class Scene; }
+
+namespace graphics::loaders
+{
+	class TextureLoader
+	{
+    public:
+        TextureLoader(rhi::vulkan::GpuDeviceVulkan& device);
+        ~TextureLoader();
+
+        void LoadSceneTextures(const graphics::scene::Scene& scene,
+            std::vector<resources::TextureGpu>& texturesOut);
+
+        resources::TextureGpu CreateWhiteFallback();
+
+    private:
+        resources::TextureGpu LoadTextureFile(const std::string& path, bool srgb = true);
+
+        rhi::vulkan::GpuDeviceVulkan& m_device;
+	};
+}
+
+#endif //VRAKTAL_GRAPHICS_LOADERS_TEXTURE_LOADER_H
