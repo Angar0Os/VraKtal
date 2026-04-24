028711b687ec2cb1da6f5bc8bcd5a9f1d61bda5e
Noukho <gaetan.payen@ynov.com>
2026-04-24 12:58:51 +0200

    wip cursor + first cell
    correction bug
    Curseur fonctionelle
    Début de la création des cells

--- VraKtalEditor/include/TimelineEditor.h
+++ VraKtalEditor/include/TimelineEditor.h
@@ -3,6 +3,9 @@
 #include "SyncEngine.h"
 #include "imgui/imgui.h" 
 
+struct {
+	
+}Cell;
 
 
 class TimelineEditor
@@ -17,18 +20,33 @@ public:
 
 	void TimelineWidget(const char* str_id, ImVec2 size);
 
+	//timeline component
 	void KeyframeExplorer(const char* str_id, ImVec2 size);
-
 	void MusicTrackSlider(const char* str_id, float* current_time, float duration, ImVec2 size);
+	void Timelinevisualizer(const char* str_id, ImVec2 size);
 
-	void Timelinevizualizer(const char* str_id, ImVec2 size);
+
+	//vizualizer component
+	void cell(ImVec2i position);
+	void Cursor(const char* str_id, ImVec2 size);
+
 
 	std::string ConvertToTime(float time);
 
 private:
 	std::string loadedSong;
 	AudioManager* audio;
+	
 	float volume;
+	
+	//cell and visualizer value
 	float zoomLevel;
+	ImVec2 cellSize;
+	std::vector<std::vector<Cell>> cellGrid;
+
+	//cursor value
+	ImVec2 cursorPos;
+	ImU32 cursorCol;
+	float cursorSize;
 
 };
--- VraKtalEditor/src/TimelineEditor.cpp
+++ VraKtalEditor/src/TimelineEditor.cpp
@@ -106,7 +106,7 @@ void TimelineEditor::TimelineWidget(const char* str_id, ImVec2 size)
 
 			KeyframeExplorer("keyframeExplorer", keyframeExplorerZone);
 			ImGui::SameLine();
-			Timelinevizualizer("timeline", ImGui::GetContentRegionAvail());
+			Timelinevisualizer("timeline", ImGui::GetContentRegionAvail());
 
 		ImGui::PopID();
 
@@ -148,14 +148,15 @@ void TimelineEditor::MusicTrackSlider(const char* str_id, float* current_time, f
 	const ImGuiID id = window->GetID(str_id);  //ImGui::GetContentRegionAvail()
 
 	ImVec2 pos = window->DC.CursorPos;
-	ImRect bb(pos, ImVec2(pos.x + size.x, pos.y + size.y));
+	ImRect bb(pos, ImVec2(size.x,size.y));
 
 	// 1. On réserve l'espace dans le layout et on gère l'interaction
 	ImGui::ItemSize(bb);
 	if (!ImGui::ItemAdd(bb, id)) return;
 
 	bool hovered, held;
-	bool pressed = ImGui::ButtonBehavior(bb, id, &hovered, &held);
+	ImRect bbBehavior(pos, ImVec2(bb.Max.x + bb.Min.x, bb.Max.y + bb.Min.y));
+	bool pressed = ImGui::ButtonBehavior(bbBehavior, id, &hovered, &held);
 
 	// 2. Logique de mise à jour (Drag)
 	if (held)
@@ -177,17 +178,24 @@ void TimelineEditor::MusicTrackSlider(const char* str_id, float* current_time, f
 	draw_list->AddRectFilled(bb.Min, ImVec2(grab_x, bb.Max.y), ImGui::GetColorU32(ImGuiCol_SliderGrabActive), 5.0f);
 
 		// Le curseur (Grab)
+	cursorPos = ImVec2(grab_x, bb.GetCenter().y);
 	if (hovered || held)
 	{
-		draw_list->AddCircleFilled(ImVec2(grab_x, bb.GetCenter().y), 8.0f, IM_COL32(255, 255, 255, 255));
+		cursorCol = IM_COL32(255, 100, 0, 255);
+		cursorSize = 2.5;
 	}
-
+	else
+	{
+		cursorSize = 1.5;
+		cursorCol = IM_COL32(255, 0, 0, 200);
+	}
+	draw_list->AddRectFilled(ImVec2(cursorPos.x - cursorSize, bb.Min.y), ImVec2(cursorPos.x + cursorSize, bb.Max.y), cursorCol);
 	//4. temps de la musique
 	std::string time = ConvertToTime(*current_time);
 
 }
 
-void TimelineEditor::Timelinevizualizer(const char* str_id, ImVec2 size)
+void TimelineEditor::Timelinevisualizer(const char* str_id, ImVec2 size)
 {
 	ImGuiWindow* window = ImGui::GetCurrentWindow();
 	if (window->SkipItems) return;
@@ -209,25 +217,69 @@ void TimelineEditor::Timelinevizualizer(const char* str_id, ImVec2 size)
 
 	ImGui::BeginGroup();
 		MusicTrackSlider("trackSlider", &currentTime, length, musicTrackZone);
-		ImVec2 vizualizerZone = ImVec2(bb.Max.x, ImGui::GetContentRegionAvail().y);
-		//ImGui::SetCursorScreenPos(ImVec2(cpos.x, musicTrackZone.y));//+ musicTrackZone.y
-		ImGui::BeginChild("timelineVizu", vizualizerZone, ImGuiChildFlags_Border, ImGuiWindowFlags_HorizontalScrollbar);
-			ImVec2 cpos  = ImGui::GetCursorScreenPos();
-			ImVec2 ecart = ImVec2(2 * (cpos.x - bb.Min.x), 1.25 * (cpos.y - bb.Min.y ));
-			//for (int i = 0; i < 100; i++)
-				//ImGui::Text("%04d: scrollable region", i);
-				ImDrawList* draw_list = window->DrawList;
-				draw_list->AddRectFilled(cpos, ImVec2(bb.Max.x + cpos.x - ecart.x, bb.Max.y + cpos.y - ecart.y), ImGui::GetColorU32(ImGuiCol_Button));
-				//draw_list->AddRectFilled(ImVec2(bb.Min.x, bb.Min.y) , ImVec2(bb.Max.x + bb.Min.x, bb.Max.y + bb.Min.y), ImGui::GetColorU32(ImGuiCol_Button));
 
+		ImVec2 vizualizerZone = ImVec2(bb.Max.x, ImGui::GetContentRegionAvail().y);
+		ImGui::BeginChild("timelineVizu", vizualizerZone, ImGuiChildFlags_Border, ImGuiWindowFlags_HorizontalScrollbar);
+
+			ImVec2 cpos  = ImGui::GetCursorScreenPos();
+			ImVec2 ecart = ImVec2((cpos.x - bb.Min.x),(cpos.y - bb.Min.y ));
+			ImGui::SetCursorScreenPos(ImVec2(cpos.x - ecart.x, cpos.y - ecart.y + 25));
+			cpos = ImGui::GetCursorScreenPos();
+
+				ImDrawList* draw_list = window->DrawList;
+				draw_list->AddRectFilled(cpos, ImVec2(bb.Max.x + bb.Min.x, bb.Max.y + bb.Min.y), ImGui::GetColorU32(ImGuiCol_Button));
+
+				Cursor("cursor", ImVec2(bb.Max.x + bb.Min.x, bb.Max.y + bb.Min.y));
 		ImGui::EndChild();
 	ImGui::EndGroup();
 }
 
+void TimelineEditor::Cursor(const char* str_id, ImVec2 size)
+{
+	ImGuiWindow* window = ImGui::GetCurrentWindow();
+	if (window->SkipItems) return;
+
+	ImGuiContext& g = *GImGui;
+	const ImGuiStyle& style = g.Style;
+	const ImGuiID id = window->GetID(str_id);
+
+	ImVec2 pos = window->DC.CursorPos;
+	ImRect bb(cursorPos, size);
+
+	ImDrawList* draw_list = window->DrawList;
+	draw_list->AddRectFilled(ImVec2(cursorPos.x - cursorSize * 2, cursorPos.y), ImVec2(cursorPos.x + cursorSize * 2, size.y), cursorCol);
+}
+
 std::string TimelineEditor::ConvertToTime(float time)
 {
-	int Seconde = (unsigned int)time; //static_cast<int>(time);
-	int hour = time / 60;  // static_cast<int>((currentTime - hour) * 60
+	if (time < 0)
+	{
+		return "00:00";
+	}
+	else
+	{
+		int minute = time / 60;  // static_cast<int>((currentTime - hour) * 60
+		int Seconde = (unsigned int)time - (minute * 60); //static_cast<int>(time);
+		std::string strSeconde; //= std::format("{:2}:{:2}", hour, Seconde - (hour * 60));//0:.2s 1:.2s;
+		std::string strMinute;
 
-	return std::format("{:2}:{:2}", hour, Seconde - (hour * 60));//0:.2s 1:.2s
+
+		if (Seconde < 10)
+			strSeconde = std::format("0{}", Seconde);
+		else if (Seconde == 0)
+			strSeconde = std::format("00");
+		else
+			strSeconde = std::format("{:2}", Seconde);
+
+
+		if (minute)
+			strMinute = std::format("0{}", minute);
+		else if (minute == 0)
+			strMinute = std::format("00");
+		else
+			strMinute = std::format("{:2}", minute);
+
+		return strMinute + ":" + strSeconde;// std::format("{}:{}", strMinute, strSeconde);//0:.2s 1:.2s
+	}
+	
 }
